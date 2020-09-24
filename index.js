b7d8aaadf4f7bc114021e4cfd0b057c03bf7d585
akshatgarg12 <akshatarungarg78@gmail.com>
2020-09-24 19:55:43 +0530

    fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 
