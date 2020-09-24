f57509e56ac83b592d8bf264d95dfa2add37b8b8
akshatgarg12 <akshatarungarg78@gmail.com>
2020-09-24 21:15:25 +0530

    fix

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use('/topHeadline',require('./routes/topHeadline.js'));
 app.use('/specificHeadline',require('./routes/specificHeadlines.js'));
 app.use('/findarticle',require('./routes/searchArticles.js'));
 app.use('/twitter', require('./routes/twitter.js'))
-app.use('/scrape', require('./routes/ArticleScraper.js'))
+// app.use('/scrape', require('./routes/ArticleScraper.js'))
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT);
--- routes/ArticleScraper.js
+++ routes/ArticleScraper.js
@@ -1,18 +1,18 @@
-const express  = require('express');
-const router = express.Router();  
-const {
-  extract 
-} = require('article-parser');
+// const express  = require('express');
+// const router = express.Router();  
+// const {
+//   extract 
+// } = require('article-parser');
  
-async function articleParser(targetUrl){
-  const article = await extract(targetUrl).catch(e=>console.log(e));
-  return article;
-}
+// async function articleParser(targetUrl){
+//   const article = await extract(targetUrl).catch(e=>console.log(e));
+//   return article;
+// }
 
-router.post('/',async(req,res)=>{
-  let url = req.body.url;
-  let data = await articleParser(url.toString()).catch(e => console.log(e));
-  res.send(data);
-})
+// router.post('/',async(req,res)=>{
+//   let url = req.body.url;
+//   let data = await articleParser(url.toString()).catch(e => console.log(e));
+//   res.send(data);
+// })
 
-module.exports= router; 
+// module.exports= router; 
